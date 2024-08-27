97a7d4f6ded20a98a7e9529b4dca6ab08b32716b
illa1 <ilia.verstuk@gmail.com>
2024-08-27 12:59:03 +0300

    project

--- p19-20/Rediscover the world/main.py
+++ p19-20/Rediscover the world/main.py
@@ -10,6 +10,11 @@ def index():
     return render_template('index.html', n=n, k=k)
 
 
+@app.route('/<user>')
+def index_user(user):
+    return render_template('index.html', name=user)
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -22,12 +27,26 @@ def add_article():
 
 @app.route('/explore')
 def explore():
-    return render_template('articles.html')
+    links = ['Назва 1 посилання',
+            'Назва 2 посилання',
+            'Назва 2 посилання']
+
+    return render_template('articles.html', links=links)
 
 
-@app.route('/<name>')
-def test(name):
-    return f"<h1>text {name}</h1>"
+@app.route('/Details')
+def Details():
+    return render_template('details.html')
+
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
+@app.route('/hello/<name>')
+def hello_user(name):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
